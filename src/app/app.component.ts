504234a4c2e6027932b99fce3a01a4b751081c86
japhethnyaranga <nyaranga4@gmail.com>
2024-03-13 00:29:38 +0300

    🔧 created initial project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TasksComponent } from './tasks/tasks/tasks.component';
+import { CompleteComponent } from './tasks/complete/complete.component';
+import { ProgressComponent } from './tasks/progress/progress.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatGridListModule, TasksComponent, CompleteComponent, ProgressComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
